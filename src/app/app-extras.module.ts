25b7147f47abe64d311aedbca6edc775014be2e8
Emily <emily.huang@Blackbaud.com>
2021-10-04 16:43:04 -0400

    no show

--- src/app/app-extras.module.ts
+++ src/app/app-extras.module.ts
@@ -2,13 +2,23 @@ import {
   NgModule
 } from '@angular/core';
 
+import { SkyI18nModule,  SKY_LIB_RESOURCES_PROVIDERS} from '@skyux/i18n';
+
 import {
   AppSkyModule
 } from './app-sky.module';
 
+import { MyLibraryResourcesProvider } from './public/plugin-resources/my-library-resources-provider';
+
 @NgModule({
+  providers: [{
+    provide: SKY_LIB_RESOURCES_PROVIDERS,
+    useClass: MyLibraryResourcesProvider,
+    multi: true
+  }],
   exports: [
-    AppSkyModule
+    AppSkyModule,
+    SkyI18nModule
   ]
 })
 export class AppExtrasModule { }
